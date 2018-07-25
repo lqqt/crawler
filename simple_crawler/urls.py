573ac49ca95bb5a7ec1710162c82064b73737b65
Lukasz P <luqu23@gmail.com>
2018-07-25 20:22:31 +0200

    add .htmls, create simple views

--- simple_crawler/urls.py
+++ simple_crawler/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:element_id>/', views.result, name="result")
 ]
--- simple_crawler/views.py
+++ simple_crawler/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
 
+from .models import Element
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    return render(request, "simple_crawler/index.html")
+
+def result(request, element_id):
+    data = Element.objects.order_by('content')[:5]
+    return render(request, "simple_crawler/result.html", {"element_list": data})
