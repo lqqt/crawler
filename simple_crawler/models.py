991a01837d583f20254027b6e4d5488f0c829949
Lukasz P <luqu23@gmail.com>
2018-07-25 19:46:43 +0200

    modify models, admins panel

--- simple_crawler/models.py
+++ simple_crawler/models.py
@@ -6,7 +6,13 @@ from django.db import models
 class FullWebsite(models.Model):
     content = models.TextField("Content")
 
+    def __str__(self):
+        return self.content[:100]
+
 class Element(models.Model):
     address = models.URLField()
     describtion = models.CharField(max_length=300)
     content = models.ForeignKey(FullWebsite, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.address
