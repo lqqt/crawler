42a42cdf4de13b4237cfa544e7419211936fdbd3
Lukasz P <luqu23@gmail.com>
2018-07-25 19:17:37 +0200

    add simple db models

--- simple_crawler/models.py
+++ simple_crawler/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class FullWebsite(models.Model):
+    content = models.TextField("Content")
+
+class Element(models.Model):
+    address = models.URLField()
+    describtion = models.CharField(max_length=300)
+    content = models.ForeignKey(FullWebsite, on_delete=models.CASCADE)
